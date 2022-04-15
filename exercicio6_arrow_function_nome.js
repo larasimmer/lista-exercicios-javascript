06c7ac36599cdd680ed0d17404f20d9aa4fd8ae6
Lara Simmer <larafsimmer@gmail.com>
2022-04-15 18:45:10 -0300

    Exercícios 6 e 7, arrow function

---
+++ exercicio6_arrow_function_nome.js
@@ -0,0 +1,11 @@
+
+let mostrarNome = (name, lastName) => console.log("Olá " + name + " " + lastName + ", eu sou uma função!");
+
+mostrarNome("Lara", "Simmer");
+
+/////DECLARANDO FUNÇÕES
+
+//function mostrarNome(name, lastName) {
+    //console.log("Olá " + name + " " + lastName + ", eu sou uma função!");
+//}
+
---
+++ exercicio7_arrow_function_diametro.js
@@ -0,0 +1,20 @@
+let diametro = (raio) => console.log(2*raio);
+
+diametro(30);
+
+/////VARIÁVEIS
+
+//let diametro;
+
+/////CHAMANDO FUNÇÕES
+
+//descobreDiametro(10);
+
+/////DECLARANDO FUNÇÕES
+
+//function descobreDiametro(raio) {
+  //  diametro = 2*raio;
+    //console.log(diametro);
+
+    //return diametro;
+//}
